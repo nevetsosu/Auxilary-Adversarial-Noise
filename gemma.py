e21cb74561f02f49782087c4151e23848a10a4c4
Steven Dao <stevendao100@gmail.com>
2024-11-18 12:24:54 -0500

    managed to produce an adversarial image

--- gemma.py
+++ gemma.py
@@ -1,4 +1,4 @@
-from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
+from transformers import PaliGemmaProcessor, PaliGemmaForConditionalGeneration
 from PIL import Image
 import torch
 
@@ -6,14 +6,15 @@ model_id = "google/paligemma-3b-mix-224"
 device = "cuda:0"
 dtype = torch.bfloat16
 
-image = Image.open('./logos/Facebook.png').convert('RGB')
+image = Image.open('./logos/DHL.png').convert('RGB')
+image.save("./converted.png")
 model = PaliGemmaForConditionalGeneration.from_pretrained(
     model_id,
     torch_dtype=dtype,
     device_map=device,
     revision="bfloat16",
-).eval()
-processor = AutoProcessor.from_pretrained(model_id)
+)
+processor = PaliGemmaProcessor.from_pretrained(model_id)
 
 # Instruct the model to create a caption in Spanish
 prompt = " <image>Give me the logo name and only the name."
@@ -44,7 +45,7 @@ print(f'decoded prediction: {decoded}')
 #
 
 # get target inputs
-target_label = "facebook"
+target_label = "apple"
 target_inputs = processor.tokenizer(text=target_label + "<eos>", return_tensors="pt")['input_ids'][0].to(model.device)
 target_length = target_inputs.size()[0]
 
@@ -76,10 +77,53 @@ model.zero_grad()
 loss.backward()
 
 # adjust original data
-epsilon = 0.1
+epsilon = 0.01
 grad = model_inputs['pixel_values'].grad.data
 adversarial = model_inputs['pixel_values'] + epsilon * grad.sign()
 
 print(loss)
+print(f"normal pixel values: {model_inputs['pixel_values']}")
+print(f'adversarial: {adversarial}')
+
+print(processor.image_processor)
+
+from torchvision import transforms
+def tensor_to_image(processor, original_image, pixel_values):
+    image_mean = [
+        0.5,
+        0.5,
+        0.5
+    ]
+    image_std = [
+        0.5,
+        0.5,
+        0.5
+    ]
+
+    mean = torch.tensor(image_mean).view(-1,1,1).to(device)
+    std = torch.tensor(image_std).view(-1,1,1).to(device)
+    rescale = torch.tensor([255, 255, 255]).view(-1, 1, 1).to(device)
+
+    print(f"pixelvalues shape: {pixel_values.size()}")
+    denorm_img_tensor = pixel_values * std + mean
+    rescaled_img_tensor = denorm_img_tensor * rescaled_img
+    print(f"rescaled tensor values: {rescaled_img_tensor}")
+    # print(f'img_tensor preclamp: {img_tensor}')
+    img = transforms.ToPILImage()(rescaled_img_tensor.to(dtype=torch.uint8))
+    
+    width, height = original_image.size
+    resized_img = img.resize((width, height))
+    resized_img.save("./adversarial.png")
+
+    return img
+
+adv_img = tensor_to_image(processor.image_processor, image, adversarial[0])
+
+print(f"after reconstruction pixel: {list(adv_img.getdata())[0]}, pixel_value: {model_inputs['pixel_values'][0]}")
+print(f"original pixel: { list(image.getdata())[0] }, original_pixel_value: {model_inputs['pixel_values'][0]}")
+
+
+
+
 # target_caption = "<image>a cat"
 # target_inputs = processor(text=target_caption, images=image, return_tersors="pt").to(model device, )
